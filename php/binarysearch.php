34a0017470b526b61df9c7d87ff3e8a04b324014
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-01 14:57:44 +0700

    Update binarysearch.php

--- php/binarysearch.php
+++ php/binarysearch.php
@@ -2,13 +2,47 @@
 
 <head>
     <Title>Halo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
-    <?php
-    echo "juan jancok";
-    echo "juan telek0";
-    ?>
+    <div class="container p-5">
+        <form method="post" action="binarysearch.php">
+            <label for="index">Masukkan angka</label>
+            <input type="number" id="index" name="index">
+            <input type="submit">
+        </form>
+        <?php
+        function binary($array, $n)
+        {
+            $l = 0;
+            $r = count($array) - 1;
+            while ($l <= $r) {
+                $m = (floor(($l + $r) / 2));
+                if ($array[$m] == $n) {
+                    return $m;
+                }
+
+                if ($array[$m] < $n) {
+                    $l = $m + 1;
+                }
+
+                if ($array[$m] > $n) {
+                    $r = $m - 1;
+                }
+            }
+            return -1;
+        }
+        $exarr = [1, 2, 3, 4, 5];
+        $n = $_POST["index"];
+        $result = binary($exarr, $n);
+        echo $result;
+        ?>
+    </div>
+
 </body>
 
 </html>
